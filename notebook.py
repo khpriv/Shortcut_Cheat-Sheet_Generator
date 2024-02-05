f33c0edf74c857bbf57ce96df56142e625aa6477
STG\khoffma <karol.hoffmann@swtest.pl>
2024-02-05 15:17:34 +0100

    todo and class Stock

--- notebook.py
+++ notebook.py
@@ -13,3 +13,29 @@ TO DO:
 - plot of price of stock or whole portfolio
 - gui
 """
+
+from datetime import datetime
+
+
+class Stock:
+    def __init__(self, index):
+        if not index:
+            raise ValueError('Unknown index')
+        self._index = index
+        self._timestamp = datetime.timestamp(datetime.now())
+        self._price_bought = provide_price(index)
+
+    def __str__(self):
+        return 'Dont do this'
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+    @property
+    def price_bought(self):
+        return self._price_bought
+
+    def provide_price(self, index):
+        ...
+
