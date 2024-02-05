e77ce0e8728f5dc8217a89a7369dcb6232d19378
STG\khoffma <karol.hoffmann@swtest.pl>
2024-02-05 14:21:15 +0100

    - commit from elsewhere

--- notebook.py
+++ notebook.py
@@ -1,3 +1,15 @@
-print("Hello world")
-print("working branch")
-print('check this sync')
+"""
+TO DO:
+- api call to get stock price
+- data structure to store in file:
+    - name of stock
+    - initial price
+    - initial timestamp
+    - current price
+    - store historic price (or receive it)
+- total the value of single stock
+- introduce portfolio of stocks
+- total the value of all/select stocks
+- plot of price of stock or whole portfolio
+- gui
+"""
