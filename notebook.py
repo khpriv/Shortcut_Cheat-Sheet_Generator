8e655fffa7832727d137f0042d84c748a6ba9416
khoffma <evanthass@gmail.com>
2024-02-19 07:45:34 +0100

    image generation .gitignore

--- notebook.py
+++ notebook.py
@@ -1,38 +1,76 @@
 import PIL.ImageOps
 from PIL import Image, ImageDraw, ImageFont
 
-sizefinal = (1024, 1024)
+sizefinal_alt = (1024, 1024)
 filename = 'pliczek'
 margines = 20
-font_desc = ImageFont.truetype("arial.ttf", 25)
+font_size = 25
+line_spacing = 20
+default_line_height = font_size + line_spacing
+default_width_of_panel = 400
+font_desc = ImageFont.truetype("arial.ttf", font_size)
+
+description_action = (f'action\n'
+                      f'action1\n'
+                      f'action2\n'
+                      f'action3\n'
+                      f'action4\n'
+                      f'action5\n'
+                      f'action6\n'
+                      )
+description_key = (f'CTRL + K\n'
+                   f'CTRL + W\n'
+                   f'CTRL + W\n'
+                   f'CTRL + W\n'
+                   f'CTRL + W\n'
+                   f'CTRL + W\n'
+                   f'CTRL + W\n'
+                   )
 
 
 def draw_border(image, spacing):
     image = PIL.ImageOps.expand(image, spacing * 2, 'black')
     border = ImageDraw.Draw(image)
-    border.rounded_rectangle([(spacing, spacing), (image.width - spacing, image.height - spacing)], width=5, fill=None, outline='lightgrey', radius=30)
+    border.rounded_rectangle([(spacing, spacing), (image.width - spacing, image.height - spacing)],
+                             width=5,
+                             fill=None,
+                             outline='grey',
+                             radius=30)
 
     return image
 
 
-def create_panel(size=(128, 128)):
-    new_panel = Image.new('RGBA', size, 'black')
-    return new_panel
+def get_size(size=sizefinal_alt):
+    return default_width_of_panel, description_action.count('\n') * default_line_height
 
 
 def write_multiline(image):
     tekst = ImageDraw.Draw(image)
-    tekst.text((10, 25), "world", font=font_desc)
+    tekst.multiline_text((0, 0),
+                         description_action,
+                         align='left', font=font_desc,
+                         spacing=line_spacing,
+                         fill='lightgrey')
+    tekst.multiline_text((default_width_of_panel, 0),
+                         description_key,
+                         align='right',
+                         font=font_desc,
+                         anchor='ra',
+                         spacing=line_spacing)
     return image
 
 
-def main():
-    keymap = Image.new('RGBA', sizefinal, 'black')
-    panel = create_panel(sizefinal)
+def create_panel():
+    size_no_border = get_size()
+    keymap = Image.new('RGBA', size_no_border, 'black')
     keymap = write_multiline(keymap)
     keymap = draw_border(keymap, margines)
+    return keymap
 
-    keymap.save(f'{filename}.png')
+
+def main():
+    panel = create_panel()
+    panel.save(f'{filename}.png')
 
 
 if __name__ in '__main__':
--- notebook2.py
+++ notebook2.py
@@ -1,45 +1,93 @@
-import sys
-
 import PIL.ImageOps
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
+sizefinal_alt = (1024, 1024)
+filename = 'pliczek'
+margines = 20
+font_size = 25
+line_spacing = 20
+default_line_height = font_size + line_spacing
+default_width_of_panel = 400
+font_desc = ImageFont.truetype("arial.ttf", font_size)
+
+description_action = ((f'action\n'
+                       f'action1\n'
+                       f'action2\n'
+                       f'action3\n'
+                       f'action4\n'
+                       f'action5\n'
+                       f'action6\n'
+                       ),
+                      (f'action\n'
+                       f'action1\n'
+                       f'action2\n'
+                       f'action3\n'
+                       f'action4\n'
+                       f'action5\n'
+                       f'action6\n'
+                       ))
+description_key = ((f'CTRL + K\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    ),
+                   (f'CTRL + K\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    f'CTRL + W\n'
+                    ))
+
+
+def draw_border(image, spacing):
+    image = PIL.ImageOps.expand(image, spacing * 2, 'black')
+    border = ImageDraw.Draw(image)
+    border.rounded_rectangle([(spacing, spacing), (image.width - spacing, image.height - spacing)],
+                             width=5,
+                             fill=None,
+                             outline='grey',
+                             radius=30)
+
+    return image
+
+
+def get_size(size=sizefinal_alt):
+    return default_width_of_panel, description_action.count('\n') * default_line_height
+
+
+def write_multiline(image):
+    tekst = ImageDraw.Draw(image)
+    tekst.multiline_text((0, 0),
+                         description_action,
+                         align='left', font=font_desc,
+                         spacing=line_spacing,
+                         fill='lightgrey')
+    tekst.multiline_text((default_width_of_panel, 0),
+                         description_key,
+                         align='right',
+                         font=font_desc,
+                         anchor='ra',
+                         spacing=line_spacing)
+    return image
+
+
+def create_panel():
+    size_no_border = get_size()
+    keymap = Image.new('RGBA', size_no_border, 'black')
+    keymap = write_multiline(keymap)
+    keymap = draw_border(keymap, margines)
+    return keymap
 
 
 def main():
-    try:
-        shirt, before = openfile(sys.argv)
-        after_name = sys.argv[2].lower()
-    except FileNotFoundError:
-        sys.exit(f'Could not read {sys.argv[1]}')
-    size_of_shirt = shirt.size
-    after = PIL.ImageOps.fit(before, size_of_shirt)
-    # fit is done correctly
-    after.paste(shirt, (0, 0), shirt)
-    after.save(f'{after_name}')
-
-
-def openfile(filename):
-    """
-    Checks file for validity and opens it, if valid
-    """
-    if len(sys.argv) == 3:
-        try:
-            name1, extension1 = sys.argv[1].lower().strip().split('.')
-            name2, extension2 = sys.argv[2].lower().strip().split('.')
-        except ValueError:
-            sys.exit('Wrong file name')
-        if (extension1 not in ('png', 'jpg', 'jpeg')) or (extension2 not in ('png', 'jpg', 'jpeg')):
-            sys.exit('Invalid output')
-        elif extension1 != extension2:
-            sys.exit('Input and output have different extensions')
-        else:
-            shirt = Image.open('shirt.png')
-            before = Image.open(sys.argv[1])
-
-            return shirt, before
-    elif len(sys.argv) < 3:
-        sys.exit('Too few command-line arguments')
-    elif len(sys.argv) > 3:
-        sys.exit('Too many command-line arguments')
+    for keys, actions in description_key, description_action:
+        panel = create_panel()
+    panel.save(f'{filename}.png')
 
 
 if __name__ in '__main__':
